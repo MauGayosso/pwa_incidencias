9e32284eb039ddd17e3be2ba1060075ea129f416
Mauricio Guerrero Gayosso <65859942+MauGayosso@users.noreply.github.com>
2023-12-05 13:12:10 -0600

    Update getDocuments.js

--- js/getDocuments.js
+++ js/getDocuments.js
@@ -1,18 +1,20 @@
-//const { MongoClient } = require('mongodb');
+const { MongoClient } = require('mongodb');
 const uri = "mongodb+srv://user_incidencias:buCygOKS3H9E4ejq@market.phxils5.mongodb.net/"
-const client = new MongoClient(uri);
 
 async function run(){
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     try {
         await client.connect();
         const db = client.db('incidencias');
         const collection = db.collection('reportes');
 
-        const first = await collection.find();
+        const first = await collection.find({}).toArray();
+        console.log("Retrieved Docuements");
         console.log(first);
     }
     finally {
         await client.close();
+        console.log("Closed connection")
     }
 }
 run().catch(console.error);
